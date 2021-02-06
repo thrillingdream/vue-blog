d6885d75540151a432437165564928e0c1b89b57
thrillingdream <thrillingdream@gmail.com>
2021-02-07 01:03:09 +0900

    first commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+
+// module.exports = {
+//   outputDir: "../html/",
+//   assetsDir: "../assets/"
+// }
+module.exports = {
+  outputDir: '../html/',
+  assetsDir: './assets/',
+  publicPath: './'
+} 
